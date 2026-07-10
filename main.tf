28098cb3f0d5a93be0152096173c613f215ba83c
njilani21 <njilani@sunbonn.com>
2026-07-10 17:28:10 +0500

    Main file updated with resource

--- main.tf
+++ main.tf
@@ -2,10 +2,17 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
+}
 
-  required_version = ">= 1.0.0"
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "ubuntu-server"{
+  ami = "ami-0f8a61b66d1accaee" # Example AMI ID for ubuntu server in us-east-1
+  instance_type = "t3.micro"
 }
 
