f235df3769a6148fb0777e6934edd0ff9c5ee8b7
Syed Nasr <njilani21>
2026-07-20 18:49:12 +0500

    Main file got updated with the cloud backup

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
 terraform {
+  cloud {
+    organization = "nasr-jilani"
+
+    workspaces {
+      name = "aws-cloud-terraform"
+    }
+  }
+
   required_version = ">= 1.5.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
